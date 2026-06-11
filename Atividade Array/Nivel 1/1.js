26b1af1b024ab94a22cfe5031c1f04735e2848ce
Fabricio Barbosa Pimentel <Fabricio.pimentel@edu.senai.br>
2026-06-11 11:25:04 -0300

    Adicionar atividade 1

---
+++ Atividade Array/Nivel 1/1.js
@@ -0,0 +1,4 @@
+let alunos=['Fabricio', 'Igor', 'Leonardo', 'Ygor', 'Guilherme',]
+console.log (alunos)
+console.log (alunos[0]) + ('É o primeiro aluno')
+console.log (alunos[4]) + ('É o ultimo aluno')
