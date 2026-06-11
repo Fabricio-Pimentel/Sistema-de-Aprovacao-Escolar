84c1f4003f8fd3797f0d0319ffb53bdee21d74b9
Fabricio Barbosa Pimentel <Fabricio.pimentel@edu.senai.br>
2026-06-11 11:47:33 -0300

    Adicionar atividade7

---
+++ Atividade Array/Nivel 2/7.js
@@ -0,0 +1,5 @@
+let diciplina = ['Matematica', 'Portugues', 'Inglês', ]
+console.log (diciplina)
+console.log ('vamos adicionar outra diciplina')
+diciplina.push('Programação')
+console.log (diciplina)
